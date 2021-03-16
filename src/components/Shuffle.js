93c74a5f2098a660bc5f887de4a4de03ffcc0f6e
daniloorozco <daniloorozco@cmail.carleton.ca>
2021-03-16 19:28:28 -0400

    Create and init functional components

---
+++ src/components/Shuffle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from './Button';
+
+function Shuffle(){
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Shuffle;
---
+++ src/components/Solve.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from './Button';
+
+function Solve(){
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Solve;
