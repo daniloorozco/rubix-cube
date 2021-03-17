94b892ef3d17b9740e6aa2d1f458f3a967504593
daniloorozco <daniloorozco@cmail.carleton.ca>
2021-03-17 11:21:56 -0400

    Refactor button component

--- src/components/Shuffle.js
+++ src/components/Shuffle.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Button from './Button';
+import styled from 'styled-components';
+
 
 function Shuffle(){
 
@@ -8,9 +10,27 @@ function Shuffle(){
     }
     return(
         <div>
-            <Button onClick={clickHandler()} text="shuffle"></Button>
+            <StyledButton onClick={clickHandler}>Shuffle</StyledButton>
         </div>
     )
 }
+
+const StyledButton = styled.button `
+    background-color: #ff4081;
+    font-weight: bold;
+    color: white;
+    width: 100px;
+    height: 40px;
+    padding: 5px 15px;
+    border-radius: 50px;
+    outline: 0;
+    text-transform: uppercase;
+    box-shadow: 0px 2px 2px lightgray;
+    cursor: pointer;
+    transition: ease background-color 250ms;
+    &:hover {
+        background-color: #c51162;
+    }
+`
 
 export default Shuffle;
--- src/components/Solve.js
+++ src/components/Solve.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Button from './Button';
+import styled from 'styled-components';
+
 
 function Solve(){
     function clickHandler(){
@@ -7,9 +9,27 @@ function Solve(){
     }
     return(
         <div>
-            <Button onClick={clickHandler} text="solve"></Button>
+            <StyledButton onClick={clickHandler}>Solve</StyledButton>
         </div>
     )
 }
+
+const StyledButton = styled.button `
+    background-color: #ff4081;
+    font-weight: bold;
+    color: white;
+    width: 100px;
+    height: 40px;
+    padding: 5px 15px;
+    border-radius: 50px;
+    outline: 0;
+    text-transform: uppercase;
+    box-shadow: 0px 2px 2px lightgray;
+    cursor: pointer;
+    transition: ease background-color 250ms;
+    &:hover {
+        background-color: #c51162;
+    }
+`
 
 export default Solve;
